25e2c7d94d51515c12ab1a8a3c8d08690dc0388e
Landskron Vagner <vagner.landskron@elbitsystems.com>
2019-09-11 22:42:06 +0300

    [chore] working hard on models, lighting and shading.

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -9,6 +9,10 @@
 #include "CThreadHolder.h"
 #include "GL/glut.h"
 #include "CGameController.h"
+#include "glm/glm.hpp"
+#include <glm/gtc/matrix_transform.hpp> 
+#include <glm/gtc/type_ptr.hpp>
+
 
 bool 
 EngineCore::IGame::Create()
@@ -55,6 +59,7 @@ bool Game::PostRendererInitialize()
 
 void Game::UpdateObjects(float dt)
 {
+    const Float cSpeed = 30.f;
 	if (mpGameInput->m_bKey[27])
 	{
 		mGame->Quit();
@@ -62,28 +67,28 @@ void Game::UpdateObjects(float dt)
 	}
 	if (mpGameInput->m_bKey['w'])
 	{
-		Graphics::IRenderer::mRenderer->GetCamera().MoveForward(dt*1.0);
+		Graphics::IRenderer::mRenderer->GetCamera().MoveForward(dt*cSpeed);
 	} 
 	else if (mpGameInput->m_bKey['s'])
 	{
-		Graphics::IRenderer::mRenderer->GetCamera().MoveForward(-dt*1.0);
+		Graphics::IRenderer::mRenderer->GetCamera().MoveForward(-dt* cSpeed);
 	}
 	if (mpGameInput->m_bKey['d'])
 	{
-		Graphics::IRenderer::mRenderer->GetCamera().MoveRight(dt*1.0);
+		Graphics::IRenderer::mRenderer->GetCamera().MoveRight(dt*cSpeed);
 	}
 	else if (mpGameInput->m_bKey['a'])
 	{
-		Graphics::IRenderer::mRenderer->GetCamera().MoveRight(-dt*1.0);
+		Graphics::IRenderer::mRenderer->GetCamera().MoveRight(-dt* cSpeed);
 	}
 
 	if (mpGameInput->m_bKey['e'])
 	{
-		Graphics::IRenderer::mRenderer->GetCamera().MoveUpward(dt*1.0);
+		Graphics::IRenderer::mRenderer->GetCamera().MoveUpward(dt*cSpeed);
 	}
 	else if (mpGameInput->m_bKey['q'])
 	{
-		Graphics::IRenderer::mRenderer->GetCamera().MoveUpward(-dt*1.0);
+		Graphics::IRenderer::mRenderer->GetCamera().MoveUpward(-dt* cSpeed);
 	}
     
     if (mpGameInput->m_bKey['y'])
@@ -101,8 +106,8 @@ void Game::UpdateObjects(float dt)
 		const Float cosValue = cos((mpGameInput->m_movementAngle * (3.14159 / 180.F)));
 		const Float sinValue = sin((mpGameInput->m_movementAngle * (3.14159 / 180.F)));
 		//printf(" Intensity X %d Y %d cos %f sin %f\n", mpGameInput->m_movementIntensityX, mpGameInput->m_movementIntensityY, cosValue, sinValue);
-		Graphics::IRenderer::mRenderer->GetCamera().RotateX(cosValue * abs(mpGameInput->m_movementIntensityY) * dt * 10);
-		Graphics::IRenderer::mRenderer->GetCamera().RotateY(sinValue * abs(mpGameInput->m_movementIntensityX) * dt * 10);
+		Graphics::IRenderer::mRenderer->GetCamera().RotateX(cosValue * abs(mpGameInput->m_movementIntensityY) * dt * 2.5f);
+		Graphics::IRenderer::mRenderer->GetCamera().RotateY(sinValue * abs(-mpGameInput->m_movementIntensityX) * dt * 2.5f);
 	}
 
 	if (mpGameInput->m_isRightButtonPressed)
@@ -138,165 +143,215 @@ void Game::Render()
 	// adjust camera projection and view according to the current 
 	// frustum parameters (3d - perspective mode)
 	Graphics::IRenderer::mRenderer->PrepareCamera3D();
+
+    IvMatrix44 projMatrix = Graphics::IRenderer::mRenderer->GetProjectionMatrix();
+    IvMatrix44 viewMatrix = Graphics::IRenderer::mRenderer->GetViewMatrix();
+    // identity
+    glm::mat4 model = glm::mat4(1.0f);
 	
 	GLuint textureId = -1;
+
+    glPushMatrix();
+    CShaderHolder::s_pInstance->UseShaderById("textured2");
+    string texName = "brick";
+    // light set-up
+    glEnable(GL_LIGHT0);
+    m_lightAngle += 1.f;
+    static float MoveRadius = 8.F;
+    GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat light_direction[] = { 0.0, -1.0, 0.0 };
+    GLfloat light_position[] = { sin(m_lightAngle * 3.14159 / 180.F) * MoveRadius, -20.f, cos(m_lightAngle * 3.14159 / 180.F) * MoveRadius , 1.0f };
+    CShaderHolder::s_pInstance->StopShader();
+    glColor4f(1.F, 0, 0, 0.5F);
+    glBegin(GL_QUADS);
+    glVertex3f(light_position[0] - 0.1f, light_position[1], light_position[2] - 0.1f);
+    glVertex3f(light_position[0] - 0.1f, light_position[1], light_position[2] + 0.1f);
+    glVertex3f(light_position[0] + 0.1f, light_position[1], light_position[2] + 0.1f);
+    glVertex3f(light_position[0] + 0.1f, light_position[1], light_position[2] - 0.1f);
+    glEnd();
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_direction);
+
+    Int32 err = glGetError();
+    if (err != 0)
+    {
+        printf("preparing matrix error = %d\n", err);
+    }
+    
+    glPopMatrix();
 
 	glPushMatrix();
 	glEnable(GL_LIGHTING);
 	glColor4f(1.0, 0.0, 1.0, 0.5);
-	static float rotate = 0.0f;
-	rotate += 0.1f;
-	static float foolme = 0.0F;
-	static bool direction = false;
-	if (direction)
-	{
-		foolme += 0.5F;
-	}
-	else
-	{
-		foolme -= 0.5F;
-	}
-	if (foolme > 10) direction = false;
-	if (foolme < -40) direction = true;
+
 	// enable vertices array pointer rendering	
-	static SModelData m_data;
-	if (CShaderHolder::s_pInstance->UseShaderById("model"))
+    CShaderHolder::s_pInstance->UseShaderById("model");
+
+    CShaderHolder::s_pInstance->GetShaderProgramById("model")->
+        setUniformMatrix4fv("projection", 1, false, (GLfloat*)projMatrix.GetFloatPtr());
+    CShaderHolder::s_pInstance->GetShaderProgramById("model")->
+        setUniformMatrix4fv("view", 1, false, (GLfloat*)viewMatrix.GetFloatPtr());
+    CShaderHolder::s_pInstance->GetShaderProgramById("model")->
+        setUniformMatrix4fv("model", 1, false, &model[0][0]);
+
+    CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform3f("lightPos", light_position[0], light_position[1], light_position[2]);
+	CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform3f("translate", 0, 3, -200);
+	CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform3f("scale", 1.0, 1.0, 1.0);
+	CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform4f("rotation", 160.f, 0.f, 1.f, 0.f);
+	// attach a texture to the whole model
+	glEnable(GL_TEXTURE_2D);
+    CShaderHolder::s_pInstance->GetShaderProgramById("model")->setTexture("diffuseMap",
+        CTextureHolder::s_pInstance->getTextureById("water.bmp"));
+
+	CModelHolder::s_pInstance->DrawModelById("Hughes500.obj");
+
+	glDisable(GL_TEXTURE_2D);
+		
+	// deactivate this shader to not affect next rendering
+	CShaderHolder::s_pInstance->StopShader();
+
+	err = glGetError();
+	if (err != 0)
 	{
-		// light set-up
-		glEnable(GL_LIGHT0);
-
-		GLfloat lightColor0[] = { 0.8f, 0.8f, 0.0f, 1.0f }; //Color (0.5, 0.5, 0.5)
-		glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
-		GLfloat lightPos[] = { 0, 0.5, 0.8, 1.0f };
-		glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-		Int32 err = glGetError();
-		if (err != 0)
-		{
-			printf("preparing matrix error = %d\n", err);
-		}
-
-		CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform3f("translate", 0, 3, -200);
-		CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform3f("scale", 1, 1, 1);
-		CShaderHolder::s_pInstance->GetShaderProgramById("model")->setUniform4f("rotation", 160.0 + foolme, 0, 1, 0);
-
-		// attach a texture to the whole model
-		glEnable(GL_TEXTURE_2D);
-		CTextureHolder::s_pInstance->Bind("water.bmp");
-
-		CModelHolder::s_pInstance->DrawModelById("Hughes500.obj");
-
-		glDisable(GL_TEXTURE_2D);
-		
-		// deactivate this shader to not affect next rendering
-		CShaderHolder::s_pInstance->StopShader();
-
-		err = glGetError();
-		if (err != 0)
-		{
-			printf("glError Drawing Model =%d\n", err);
-		}
-		
-	}
-	
+		printf("glError Drawing Model =%d\n", err);
+	}	
 	glPopMatrix();
 
-	glPushMatrix();
-	if (CShaderHolder::s_pInstance->UseShaderById("textured2"))
-	{
-        string texName = "rock";
-		// light set-up
-		glEnable(GL_LIGHT0);
-        m_lightAngle += 0.55f;
-		static float LightMoving = 2.5F;
-		GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
-		GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
-		GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-		GLfloat light_direction[] = { 0.0, -1.0, 0.0 };
-		GLfloat light_position[] = { sin(m_lightAngle * 3.14159 / 180.F) * LightMoving, 1.f, cos(m_lightAngle * 3.14159 / 180.F) * LightMoving , 1.0f};
+    glPushMatrix();
+    CShaderHolder::s_pInstance->UseShaderById("texturedtangent");
 
-        CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setUniform3f("lightPos", light_position[0], light_position[1], light_position[2]);
 
-		glColor4f(1, 0, 0, 1);
-		glBegin(GL_QUADS);
-		glVertex3f(light_position[0] - 0.1f, light_position[1], light_position[2] - 0.1f);
-		glVertex3f(light_position[0] - 0.1f, light_position[1], light_position[2] + 0.1f);
-		glVertex3f(light_position[0] + 0.1f, light_position[1], light_position[2] + 0.1f);
-		glVertex3f(light_position[0] + 0.1f, light_position[1], light_position[2] - 0.1f);
-		glEnd();
+	glEnable(GL_TEXTURE_2D);
+	CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->setTexture("diffuseMap", 
+		CTextureHolder::s_pInstance->getTextureById(texName + "_t.bmp"));
 
-		glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-		glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-		glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_direction);
-		//glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->setTexture("normalMap",
+		CTextureHolder::s_pInstance->getTextureById(texName + "_n.bmp"));
 
-		Int32 err = glGetError();
-		if (err != 0)
-		{
-			printf("preparing matrix error = %d\n", err);
-		}
+    CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->
+        setUniformMatrix4fv("projection", 1, false, (GLfloat*)projMatrix.GetFloatPtr());
+    CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->
+        setUniformMatrix4fv("view", 1, false, (GLfloat*)viewMatrix.GetFloatPtr());
 
-		CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setUniform3f("translate", 0, 0.25, 0);
-		CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setUniform3f("scale", 1, 1, 1);
-		CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setUniform4f("rotation", 5.F /*+ foolme*/, 1, 0, 0);
+    CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->setUniform3f("lightPos", light_position[0], light_position[1], light_position[2]);
 
-		glEnable(GL_TEXTURE_2D);
-		CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setTexture("textureColor", 
-			CTextureHolder::s_pInstance->getTextureById(texName + "_t.bmp"));
-		/*if (textureId != -1)
-		{
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, textureId);1
-			glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-			CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setUniform1i("textureColor", 0);
-		}*/
-		CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setTexture("textureNormal",
-			CTextureHolder::s_pInstance->getTextureById(texName + "_n.bmp"));
-		//textureId = CTextureHolder::s_pInstance->getTextureById("brick_n.bmp");
+    CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->
+        setUniformMatrix4fv("model", 1, false, &model[0][0]);
 
-		/*if (textureId != -1)
-		{
-			glActiveTexture(GL_TEXTURE1);
-			glBindTexture(GL_TEXTURE_2D, textureId);
-			CShaderHolder::s_pInstance->GetShaderProgramById("textured2")->setUniform1i("textureNormal", 1);
-		}*/
+    CShaderHolder::s_pInstance->GetShaderProgramById("texturedtangent")->setUniform3f("viewPos", 
+        Graphics::IRenderer::mRenderer->GetCamera().m_position.GetX(),
+        Graphics::IRenderer::mRenderer->GetCamera().m_position.GetY(),
+        Graphics::IRenderer::mRenderer->GetCamera().m_position.GetZ());
 
-		GLfloat mat_shininess[] = { 1.0, 0.5, 0.31 };
-		glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_shininess);
+    static UInt32 quadVAO = 0;
+    static UInt32 quadVBO = 0;
 
-		// draw blocks of 1 meter^2 and apply texture
-		glBegin(GL_QUADS);
-		const Int32 sizeOfBlock = 2;
-		for (Int32 bx = -10; bx < 10; bx += sizeOfBlock)
-		{
-			for (Int32 by = 0; by > -20; by -= sizeOfBlock)
-			{
-				glTexCoord2f(0, 0);  glVertex3f(bx, -0.5, by);
-				glTexCoord2f(1, 0);  glVertex3f(bx + sizeOfBlock, -0.5, by);
-				glTexCoord2f(1, 1);  glVertex3f(bx + sizeOfBlock, -0.5, by + sizeOfBlock);
-				glTexCoord2f(0, 1);  glVertex3f(bx, -0.5, by + sizeOfBlock);
+    if (quadVAO == 0)
+    {
+        // positions
+        glm::vec3 pos1(-20.0f, -25.0f,  20.0f);
+        glm::vec3 pos2(-20.0f, -25.0f, -20.0f);
+        glm::vec3 pos3( 20.0f, -25.0f, -20.0f);
+        glm::vec3 pos4( 20.0f, -25.0f,  20.0f);
+        // texture coordinates
+        glm::vec2 uv1(0.0f, 1.0f);
+        glm::vec2 uv2(0.0f, 0.0f);
+        glm::vec2 uv3(1.0f, 0.0f);
+        glm::vec2 uv4(1.0f, 1.0f);
+        // normal vector
+        glm::vec3 nm(0.0f, 1.0f, 0.0f);
 
-			}
-		}
-		glEnd();
+        // calculate tangent/bitangent vectors of both triangles
+        glm::vec3 tangent1, bitangent1;
+        glm::vec3 tangent2, bitangent2;
+        // triangle 1
+        // ----------
+        glm::vec3 edge1 = pos2 - pos1;
+        glm::vec3 edge2 = pos3 - pos1;
+        glm::vec2 deltaUV1 = uv2 - uv1;
+        glm::vec2 deltaUV2 = uv3 - uv1;
 
-		CShaderHolder::s_pInstance->StopShader();
-		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-		glDisable(GL_TEXTURE_2D);
-		glBindTexture(GL_TEXTURE_2D, 0);
-		glActiveTexture(GL_TEXTURE0);
-	}
-	glPopMatrix();
+        GLfloat f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
 
+        tangent1.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent1.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent1.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+        tangent1 = glm::normalize(tangent1);
+
+        bitangent1.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent1.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent1.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+        bitangent1 = glm::normalize(bitangent1);
+
+        // triangle 2
+        // ----------
+        edge1 = pos3 - pos1;
+        edge2 = pos4 - pos1;
+        deltaUV1 = uv3 - uv1;
+        deltaUV2 = uv4 - uv1;
+
+        f = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV2.x * deltaUV1.y);
+
+        tangent2.x = f * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+        tangent2.y = f * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+        tangent2.z = f * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+        tangent2 = glm::normalize(tangent2);
+
+
+        bitangent2.x = f * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+        bitangent2.y = f * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+        bitangent2.z = f * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+        bitangent2 = glm::normalize(bitangent2);
+
+
+        float quadVertices[] = {
+            // positions            // normal         // texcoords  // tangent                          // bitangent
+            pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+            pos2.x, pos2.y, pos2.z, nm.x, nm.y, nm.z, uv2.x, uv2.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+            pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent1.x, tangent1.y, tangent1.z, bitangent1.x, bitangent1.y, bitangent1.z,
+
+            pos1.x, pos1.y, pos1.z, nm.x, nm.y, nm.z, uv1.x, uv1.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+            pos3.x, pos3.y, pos3.z, nm.x, nm.y, nm.z, uv3.x, uv3.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z,
+            pos4.x, pos4.y, pos4.z, nm.x, nm.y, nm.z, uv4.x, uv4.y, tangent2.x, tangent2.y, tangent2.z, bitangent2.x, bitangent2.y, bitangent2.z
+        };
+        // configure plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(8 * sizeof(float)));
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 14 * sizeof(float), (void*)(11 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+	CShaderHolder::s_pInstance->StopShader();
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glActiveTexture(GL_TEXTURE0);
 	glDisable(GL_LIGHTING);
-
+    glPopMatrix();
 
 	// [ TEXTURED SQUARE ]
 	glPushMatrix();
-	glEnable(GL_TEXTURE_2D);
-	
+	glEnable(GL_TEXTURE_2D);	
 	CTextureHolder::s_pInstance->Bind("brick_t.bmp");
-
+    glColor4f(1, 0, 1, 1);
 	glEnable(GL_BLEND);
 	glEnable(GL_ALPHA_TEST);
 	glAlphaFunc(GL_GREATER, 0.5f);
@@ -314,7 +369,8 @@ void Game::Render()
 
 	// [ ROTATING TRIANGLE ]
 	glPushMatrix();
-	glRotatef(rotate, 0, 0, 1);
+    static Int32 inc = 0;
+	glRotatef(160.f + ++inc, 0, 0, 1);
 	glBegin(GL_TRIANGLES);
 	glVertex3f(0, 0, -1000);
 	glVertex3f(100, 50, -1000);
