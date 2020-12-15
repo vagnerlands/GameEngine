08dbe3e16a9e5180a6389a49c93d1a6de5b2d031
vagnerlands <vagnerlands@gmail.com>
2020-12-15 22:27:40 +0200

    compilation issue
    Former-commit-id: d6193db7175a7fccb01d9d4e613d27d1badc5d69

--- Graphics/Scene/SceneItem.h
+++ Graphics/Scene/SceneItem.h
@@ -78,5 +78,55 @@ namespace Graphics
 	};
 }
 
+inline const IvVector3& Graphics::SceneItem::GetLocation() const
+{
+	return m_location;
+}
+
+inline const IvVector3& Graphics::SceneItem::GetScale() const
+{
+	return m_scale;
+}
+
+inline const IvQuat& Graphics::SceneItem::GetRotation() const
+{
+	return m_rotation;
+}
+
+inline const std::string& Graphics::SceneItem::GetId() const
+{
+	return m_sceneItemId;
+}
+
+inline const bool Graphics::SceneItem::HasShadows() const
+{
+	return m_hasShadow;
+}
+
+inline void Graphics::SceneItem::SetLocation(const IvVector3& newLocation)
+{
+	m_location = newLocation;
+}
+
+inline void Graphics::SceneItem::SetScale(const IvVector3& newDimension)
+{
+	m_scale = newDimension;
+}
+
+inline void Graphics::SceneItem::SetRotation(const IvQuat& newRotation)
+{
+	m_rotation = newRotation;
+}
+
+inline void Graphics::SceneItem::SetId(const std::string& id)
+{
+	m_sceneItemId = id;
+}
+
+inline void Graphics::SceneItem::SetCastShadows(bool castShadows)
+{
+	m_hasShadow = castShadows;
+}
+
 
 #endif // _SceneItem_H_
