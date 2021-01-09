cf9bfb710973ce0f1e0b4dc72a766949a8cd8617
Vagner P Landskron <vagnerlands@gmail.com>
2021-01-09 18:54:05 +0200

    [chore] adding a holder for particles

---
+++ EngineCore/Holders/CParticlesSystemHolder.cpp
@@ -0,0 +1,107 @@
+#include "CParticlesSystemHolder.h"
+#include "gl/glut.h"
+#include <time.h>
+#include <iostream>
+#include <string>
+#include "MutexFactory.h"
+
+#include "CParticle.h"
+
+CParticlesSystemHolder* CParticlesSystemHolder::s_pInstance = NULL;
+
+bool CParticlesSystemHolder::Create()
+{
+	return false;
+}
+
+CParticlesSystemHolder::~CParticlesSystemHolder()
+{
+	while (!m_mapParticles.empty())
+	{
+		if (m_mapParticles.begin()->second != NULL)
+			delete m_mapParticles.begin()->second;
+		m_mapParticles.erase(m_mapParticles.begin());
+	}	
+}
+
+bool CParticlesSystemHolder::Create(const string& pathToModelFile)
+{
+	if (s_pInstance == nullptr)
+	{
+		// creates instance of CParticlesSystemHolder
+		s_pInstance = new CParticlesSystemHolder(pathToModelFile);
+		// tries to open the resource file - out of the constructor so errors may be reported
+		if (s_pInstance != nullptr) /*&& (s_pInstance->m_modelFiles->VOpen()))*/
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void 
+CParticlesSystemHolder::OnRemoveEvent(const string& removeItem)
+{
+	s_pInstance->RemoveParticle(removeItem);
+}
+
+CParticlesSystemHolder::CParticlesSystemHolder(const string& pathToResources)
+	/*: m_modelFiles(new CResourceZipFile(pathToResources.data(), this->OnRemoveEvent))*/
+{
+	m_pParticlesContentMapMutex = MutexFactory::Instance().Create("ParticlesContentMapMutex");
+}
+
+
+void 
+CParticlesSystemHolder::RemoveParticle(const string& textId)
+{
+	DisplayObject::iterator it = m_mapParticles.find(textId);
+	if (it == m_mapParticles.end())
+	{
+		// this shouldn't happen - never, but if happens, trying 
+		// to erase will cause an exception - so must quit method
+		return;
+	}
+
+	if (it->second != NULL)
+	{
+		delete it->second;
+	}
+
+	m_mapParticles.erase(it);
+
+	//_CrtDumpMemoryLeaks();
+}
+
+Graphics::IDrawable*
+CParticlesSystemHolder::GetParticleById(const string& particleId)
+{
+	Graphics::IDrawable* pRet = (Graphics::IDrawable*)(NULL);
+	// then try to find it in the textures map
+	DisplayObject::iterator result = m_mapParticles.find(particleId);
+	if (result != m_mapParticles.end())
+	{
+		pRet = result->second;
+	}
+	else
+	{
+		// cache miss - then create a particle system
+		Graphics::CParticle * pParticle = new Graphics::CParticle(particleId);
+		pParticle->Create();
+		m_mapParticles.insert(make_pair(particleId, pParticle));
+		pRet = pParticle;
+	}
+
+	// if it somehow failed, returns -1
+	return pRet;
+}
+
+void CParticlesSystemHolder::Update(float dt)
+{
+	// update all animations related to each model
+	for (auto& it : m_mapParticles) 
+	{		
+		it.second->Update(dt);
+	}
+}
+
---
+++ EngineCore/Holders/CParticlesSystemHolder.h
@@ -0,0 +1,50 @@
+#ifndef CPARTICLESMANAGER_H_
+#define CPARTICLESMANAGER_H_
+
+#include "CommonTypes.h"
+#include "CResHandle.h"
+#include "IMutex.h"
+#include <unordered_map>
+#include <memory>
+#include "IDrawable.h"
+
+using namespace Types;
+using namespace std;
+
+class CParticlesSystemHolder
+{
+public:
+
+	//typedef unordered_map<string, SModelData> ModelMap;
+	typedef unordered_map<string, Graphics::IDrawable*> DisplayObject;
+
+	// always create before using
+	static bool Create();
+	virtual ~CParticlesSystemHolder();
+
+	bool Create(const string& pathToModelFile);
+    
+	void RemoveParticle(const string& particleId);
+	Graphics::IDrawable* GetParticleById(const string& particleId);
+
+	void Update(float dt);
+
+	// external callback event in case a resource is deallocated
+	static void OnRemoveEvent(const string& removeItem);
+	// local instance
+	static CParticlesSystemHolder* s_pInstance;
+
+private:
+	CParticlesSystemHolder(const string& pathToResources);
+
+	// hash map for the raw data models (vertexes, normals, material, etc)
+	//ModelMap m_models;
+	// hash map for the IModels* - containing what you need to render this model
+	DisplayObject m_mapParticles;
+	// mutex for m_processes
+	IMutex* m_pParticlesContentMapMutex;
+
+
+};
+
+#endif //CPARTICLESMANAGER_H_
